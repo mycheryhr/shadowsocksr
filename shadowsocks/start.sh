af11795bf51f6c6e7c767f66ea8a612f2d2b5f74
mycheryhr <mycheryhr@gmail.com>
2018-03-15 10:56:23 +0800

    Update start.sh

--- shadowsocks/start.sh
+++ shadowsocks/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin
 
-python server.py -p 250 -k Shelly123456 -m aes-256-cfb -O auth_sha1_v4 -o http_simple -d start > /dev/null 2>&1
+python server.py -p 33520 -k Shelly123456 -m aes-256-cfb -O auth_sha1_v4 -o http_simple -d start > /dev/null 2>&1
